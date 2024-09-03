0825b45cc3f9becadc8f93426483986e21a92cf9
Augusto <v493707@gmail.com>
2024-09-03 14:19:22 -0300

    Resolvido conflito no arquivo Home/index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,20 @@
 import { useRef } from "react";
 import api from "../../services/api";
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button/styles";
-=======
-import  {Button}  from "../../components/Button/styles";
->>>>>>> 76d22f992e40bfce33374361f92747276953da81
-
 import {
   Tilte,
   Container,
   Form,
   ContainerInput,
   Input,
-<<<<<<< HEAD
   InputLabel,
 } from "./styles";
 
-=======
-  InputLabel
-} from "./styles";
-
-
->>>>>>> 76d22f992e40bfce33374361f92747276953da81
 function Home() {
   const inputName = useRef();
   const inputAge = useRef();
   const inputEmail = useRef();
-
-<<<<<<< HEAD
   const navigate = useNavigate();
 
   async function registerNewUser() {
@@ -37,15 +23,6 @@ function Home() {
       age: parseInt(inputAge.current.value),
       name: inputName.current.value,
     });
-=======
-  async function registerNewUser() {
-    await api.post('/usuarios', {
-      email: inputEmail.current.value,
-      age: parseInt(inputAge.current.value),
-      name: inputName.current.value
-    });
-
->>>>>>> 76d22f992e40bfce33374361f92747276953da81
   }
 
   return (
@@ -57,18 +34,15 @@ function Home() {
             <InputLabel>
               Nome <span>*</span>
             </InputLabel>
-
             <Input type="text" placeholder="Nome do usuário" ref={inputName} />
           </div>
-
           <div>
             <InputLabel>
               Idade <span>*</span>
             </InputLabel>
-
             <Input
               type="number"
-              placeholder="Idade so usuário"
+              placeholder="Idade do usuário"
               ref={inputAge}
             />
           </div>
@@ -79,30 +53,15 @@ function Home() {
           </InputLabel>
           <Input type="email" placeholder="Email do Usuário" ref={inputEmail} />
         </div>
-
-<<<<<<< HEAD
         <Button type="button" onClick={registerNewUser} theme="primary">
           Cadastrar Usuário
         </Button>
-
-        <Button type="button" onClick={()=> navigate('/lista-de-usuarios')}>Ver Lista de Usuários</Button>
-=======
-        <Button type="button" onClick={registerNewUser}>
-          Cadastrar Usuário
+        <Button type="button" onClick={() => navigate('/lista-de-usuarios')}>
+          Ver Lista de Usuários
         </Button>
->>>>>>> 76d22f992e40bfce33374361f92747276953da81
       </Form>
     </Container>
   );
 }
 
 export default Home;
-<<<<<<< HEAD
-=======
-
-/*
-EXPORTAR DEFAULT "Padrão" -> uma coisa só por página
-
-Apenas EXPORTAR -> pode exportar várias
-*/
->>>>>>> 76d22f992e40bfce33374361f92747276953da81
